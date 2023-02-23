f64e4638f8d8b5d5ff911babff1bdf0613b739c0
totshe <126175086+totshe@users.noreply.github.com>
2023-02-23 19:09:34 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Env
-MY_IP=$(ip addr show | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
-TOKEN=$(grep $MY_IP "/root/er/tokens.txt" | awk '{print $4}')
-
-
-#script
-sed -i 's/zalupka/$TOKEN/g' /root/My-App/bot.py
