cdbdb3035265a89b84cfa5ab62d8118495fa4235
totshe <126175086+totshe@users.noreply.github.com>
2023-02-24 20:48:16 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 #Env
 MY_IP=$(ip addr show | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
-TOKEN=$(grep $MY_IP "/root/er/tokens.txt" | awk '{print $4}')
+TOKEN=$(grep -w $MY_IP "/root/er/tokens.txt" | awk '{print $4}')
 
 # Команды для выполнения
 cmds=(
