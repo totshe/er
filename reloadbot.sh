a7781f8d7570d8bcec0af44f9c5b197a813869ad
totshe <126175086+totshe@users.noreply.github.com>
2023-03-05 21:05:29 +0300

    Update reloadbot.sh

--- reloadbot.sh
+++ reloadbot.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 #ENV
-OLDTOKEN=$(head -n1 /root/er/tokens.txt)
-NEWTOKEN=$(head -n2 /root/er/tokens.txt | tail -1)
+OLDTOKEN=$(head -n1 /root/My-App/tokens.txt)
+NEWTOKEN=$(head -n2 /root/My-App/tokens.txt | tail -1)
 #Script
-if pgrep -f /root/er/bot.py > /dev/null
+if [ -f /root/pathfinder/py/mainnet.sqlite-wal ]
 then
-   echo "Команда 'bot.py' уже запущена.Выключаю бота..."
-   kill -9 $(ps aux | grep /root/er/bot.py | awk '{print $2}' | head -1)
+   echo "Файл есть.Очищаю файл..."
    rm -rf /root/pathfinder/py/mainnet.sqlite
-   sleep 2
-   source /root/test/my_env/bin/activate
-   python /root/er/bot.py
+   rm -rf /root/pathfinder/py/mainnet.sqlite-wal
 else
-   echo "'bot.py' не запущен, включаю бота..."
-   source /root/test/my_env/bin/activate
-   python /root/er/bot.py
-
+   echo "Файл отсутствует"
+   rm -rf /root/pathfinder/py/mainnet.sqlite
 fi
+
